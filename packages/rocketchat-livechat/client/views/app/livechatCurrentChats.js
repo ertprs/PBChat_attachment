d246183e39f89a30c1d7e18aca7b88f17ec61fc8
jitendras <jitendras@policybazaar.com>
2016-12-05 15:49:33 +0530

    Filter revamp for Date time and Select text instead of blank!!!

--- packages/rocketchat-livechat/client/views/app/livechatCurrentChats.js
+++ packages/rocketchat-livechat/client/views/app/livechatCurrentChats.js
@@ -98,3 +98,16 @@ Template.livechatCurrentChats.onCreated(function() {
 		this.subscribe('livechat:rooms', this.filter.get(), 0, this.limit.get());
 	});
 });
+
+Template.livechatCurrentChats.onRendered(function() {
+     $('#datetimepicker6').datetimepicker();
+        $('#datetimepicker7').datetimepicker({
+            useCurrent: false //Important! See issue #1075
+        });
+        $("#datetimepicker6").on("dp.change", function (e) {
+            $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+        });
+        $("#datetimepicker7").on("dp.change", function (e) {
+            $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+        });
+});
