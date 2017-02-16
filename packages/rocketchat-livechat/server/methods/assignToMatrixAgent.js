6c536a0325bd1f3e6446b78873dfa9eb9f1cd3f8
Deepankar <deepankar@policybazaar.com>
2017-02-16 19:52:05 +0530

    * changes for --> leadtype changes in ctc api

--- packages/rocketchat-livechat/server/methods/assignToMatrixAgent.js
+++ packages/rocketchat-livechat/server/methods/assignToMatrixAgent.js
@@ -3,7 +3,7 @@ Meteor.methods({
     'livechat:assignToMatrixAgent' (leadid, employeeid) {
         console.log(leadid + ' assigned to ' + employeeid);
         this.unblock();
-        var url = RocketChat.settings.get('CTCAPI') + "/CTCRestService.svc/AssignCTC/" + leadid + employeeid + "/0/0/2/0";
+        var url = RocketChat.settings.get('CTCAPI') + "/CTCRestService.svc/AssignCTC/" + leadid + "/" + employeeid + "/0/0/7/0";
         HTTP.call("GET", url, { headers: { "accept": "application/json" } });
     }
 });
