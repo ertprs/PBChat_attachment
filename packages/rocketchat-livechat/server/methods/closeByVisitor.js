84be723004eb501e956ea902dda7d7dae795808e
Deepankar Singh <deepankar080892@gmail.com>
2017-08-18 13:45:46 +0530

    changes for close chat client side

--- packages/rocketchat-livechat/server/methods/closeByVisitor.js
+++ packages/rocketchat-livechat/server/methods/closeByVisitor.js
@@ -1,23 +1,22 @@
 Meteor.methods({
-	'livechat:closeByVisitor'() {
-		if (!Meteor.userId()) {
-			throw new Meteor.Error('error-not-authorized', 'Not authorized', { method: 'livechat:closeByVisitor' });
-		}
+    'livechat:closeByVisitor' (rid) {
+        if (!Meteor.userId()) {
+            throw new Meteor.Error('error-not-authorized', 'Not authorized', { method: 'livechat:closeByVisitor' });
+        }
+        //const room = RocketChat.models.Rooms.findOneOpenByVisitorId(Meteor.userId());
+        var room = RocketChat.models.Rooms.findOne({ _id: rid });
+        if (!room || !room.open) {
+            return false;
+        }
 
-		const room = RocketChat.models.Rooms.findOneOpenByVisitorId(Meteor.userId());
+        const user = Meteor.user();
 
-		if (!room || !room.open) {
-			return false;
-		}
+        let language = (user && user.language) || RocketChat.settings.get('language') || 'en';
 
-		const user = Meteor.user();
-
-		let language = (user && user.language) || RocketChat.settings.get('language') || 'en';
-
-		return RocketChat.Livechat.closeRoom({
-			user: user,
-			room: room,
-			comment: TAPi18n.__('Closed_by_visitor', { lng: language })
-		});
-	}
+        return RocketChat.Livechat.closeRoom({
+            user: user,
+            room: room,
+            comment: TAPi18n.__('Closed_by_visitor', { lng: language })
+        });
+    }
 });
